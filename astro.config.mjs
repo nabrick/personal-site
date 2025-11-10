c5f5f773d7ce5d2eede0b7d760651252d0421ab7
Erick Holgado Bardales <erick.holgado@utec.edu.pe>
2025-11-09 20:23:31 -0500

    update & despliegue page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://github.com/nabrick/personal-site',
+  base: 'personal-site/',                            
+  output: 'static',
+
   devToolbar: {enabled: false},
 
   vite: {
